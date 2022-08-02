90aefb20effb6583e1aa315abf9cefc68a27c87d
BoYaReN <Zhtvxfr063>
2022-08-02 16:16:04 +0300

    First Task done!

--- app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -1,37 +1,57 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextWatcher
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import android.view.ViewParent
+import android.widget.*
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import java.util.*
+import androidx.lifecycle.ViewModel
+
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var email: EditText
     private lateinit var pass: EditText
+    private lateinit var language_spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+//        val languageArray = resources.getStringArray(R.array.language)
+//        language_spinner = findViewById(R.id.language_spinnerx)
+//        if(language_spinner != null){
+//            val adapter = ArrayAdapter(this,
+//                android.R.layout.simple_spinner_item, languageArray)
+//            language_spinner.adapter = adapter
+//        }
+//
+//        language_spinner.onItemSelectedListener = object :
+//            AdapterView.OnItemSelectedListener {
+//                override fun onItemSelected(parent: AdapterView<*>,
+//                                            view: View, position: Int, id: Long) {
+//                    Toast.makeText(this@RegisterActivity,
+//                        languageArray[position], Toast.LENGTH_SHORT).show()
+//                }
+//
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//            }
+//        }
+
+
         email = findViewById(R.id.email)
         pass = findViewById(R.id.pass)
 
-        email.addTextChangedListener(){
-            email.setHintTextColor(Color.parseColor("#A8A8A8"))
-            email.setHint("email:")
-        }
-
-        pass.addTextChangedListener(){
-            pass.setHintTextColor(Color.parseColor("#A8A8A8"))
-            pass.setHint("pass:")
-        }
     }
 
     fun singUp(view: View) {
+
         val main = Intent(this, MainActivity::class.java)
         var text_email = email.text.toString()
         var text_pass = pass.text.toString()
@@ -41,15 +61,11 @@ class RegisterActivity : AppCompatActivity() {
                 startActivity(main)
             }
             else{
-                pass.setText("")
-                pass.hint = "Wrong pass!"
-                pass.setHintTextColor(Color.parseColor("#FF0000"))
+                pass.error = "Wrong pass!"
             }
         }
         else{
-            email.setText("")
-            email.hint = "Wrong email!"
-            email.setHintTextColor(Color.parseColor("#FF0000"))
+            email.error = "Wrong email!"
         }
 
     }
